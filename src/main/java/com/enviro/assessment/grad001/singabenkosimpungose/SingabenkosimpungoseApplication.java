eec2ae12468c370fb953158d9ad945735dc92930
Singabenkosi Mpungose <mpungosesingabenkosi@gmail.com>
2025-01-21 12:11:02 +0200

    Initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/singabenkosimpungose/SingabenkosimpungoseApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.singabenkosimpungose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingabenkosimpungoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingabenkosimpungoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/singabenkosimpungose/SingabenkosimpungoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.singabenkosimpungose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingabenkosimpungoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
